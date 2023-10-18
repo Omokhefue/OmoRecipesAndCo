5d69f46381e58aef3431b7ac32461732ca164068
Omokhefue <mokhefue@gmail.com>
2023-10-18 11:42:37 +0100

    App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,51 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  useNavigation,
 } from "react-router-dom";
-import Home from "./pages/Homepage";
-import About from "./pages/About";
 
 import RootLayout from "./layout/RootLayout";
+import Category from "./pages/Category";
+import { Homepage } from "./pages/Homepage";
+import Recipe from "./pages/Recipe";
+import SingleCategory, { SingleCategoryLoader } from "./pages/SingleCategory";
+import { CategoriesLoader } from "./components/category/CategoryPreview";
+import { RecipesLoader } from "./components/recipe/RecipePreview";
+import {
+  SingleRecipeLoader,
+} from "./components/recipe/RecipeDetails";
+import RecipeComments, { RecipeCommentsLoader } from "./pages/RecipeComments";
+import SingleRecipeLayout from "./layout/SingleRecipeLayout";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
+      <Route
+        index
+        element={<Homepage />}
+        // loader={(RecipesLoader, CategoriesLoader)}
+        loader={RecipesLoader}
+      />
+      <Route
+        path="categories"
+        element={<Category />}
+        loader={CategoriesLoader}
+      />
+      <Route path="recipes" element={<Recipe />} loader={RecipesLoader} />
+      <Route
+        path="categories/:id"
+        element={<SingleCategory />}
+        loader={SingleCategoryLoader}
+      />
+      <Route
+        path="recipes/:id"
+        element={<SingleRecipeLayout />}
+        loader={SingleRecipeLoader}
+      >
+        <Route
+          path="comments"
+          element={<RecipeComments />}
+          loader={RecipeCommentsLoader}
+        />
+      </Route>
     </Route>
   )
 );
