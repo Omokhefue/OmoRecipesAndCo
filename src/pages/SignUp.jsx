7772251a41ec8d1bc00b5ec1441f068aba325d59
Omokhefue <mokhefue@gmail.com>
2023-10-20 18:42:12 +0100

    signup page

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,55 @@
+const SignUp = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const userInfo = Object.fromEntries(formData);
+    console.log(userInfo);
+    const res = await fetch("http://localhost:5000/api/v1/auth/signup/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+    console.log(res);
+    if (!res.ok) {
+      throw Error("there was an error signing up. Please try again");
+    }
+    const data = await res.json();
+    localStorage.setItem("access_token", data.token);
+  };
+  return (
+    <>
+      <div className="user-registration-form">
+        <h1>User Registration</h1>
+        <form onSubmit={handleSubmit}>
+
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              minLength="6"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="role">Role:</label>
+            <select name="role" id="role">
+              <option value="user">User</option>
+              <option value="admin">Admin</option>
+            </select>
+          </div>
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default SignUp;
